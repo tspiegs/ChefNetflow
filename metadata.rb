e9ebe1c86cb7f11a618bf8038f0597547099e60d
tspiegs work laptop <tspiegelman@amplify.com>
2015-05-16 10:20:02 -0400

    fixing metadata

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,10 @@
 name             'netflow'
-maintainer       'Amplify'
-maintainer_email 'syseng@amplify.com'
+maintainer       'tspiegs'
 license          'All rights reserved'
 description      'Installs/Configures netflow'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-#server side requires aws and ebs
+#server side requires aws and ebs, comment that out if you don't wanna EBS back
 
 version          '0.1.7'
 depends 'aws'
